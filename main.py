48154014cd6f40783b558f127b04ae17da42f842
hugoms0 <hugom.s.2002@gmail.com>
2025-03-11 14:12:01 +0100

    added gitignore, program now writes to a file

--- main.py
+++ main.py
@@ -3,14 +3,14 @@ import time
 
 def main():
     start = time.time()
-    max = 10000
+    max = 350
     fizzbuzz_list = [
         ["fizz", 3],
         ["buzz", 5],
-	    ["rezz", 23]
+        ["rezz", 23]
     ]
     fizzbuzzed_list = []
-    for i in range(1, max):
+    for i in range(1, max + 1):
         name = ""
         for tups in fizzbuzz_list:
             if i % tups[1] == 0:
@@ -19,7 +19,13 @@ def main():
             fizzbuzzed_list.append(i)
         else:
             fizzbuzzed_list.append(name)
-    print(fizzbuzzed_list)
+    
+    with open("fizzbuzz_output.txt", "w") as file:
+        for item in fizzbuzzed_list:
+            file.write(f"{item}\n")
+
+    # print(fizzbuzzed_list)
+
     end = time.time()
     time_elapsed = end - start
     print(f"Execution time was: {time_elapsed} seconds")
